3e4a19082b615e09bba8272550c5a53e51859759
yb805 <84612961+yb805@users.noreply.github.com>
2021-06-13 18:07:07 +0900

    Add files via upload

---
+++ Spin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float rotSpeed = 80f;
+
+
+    
+    
+    void Update ()
+    {
+        transform.Rotate(new Vector3(0,rotSpeed * Time.deltaTime, 0));
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            
+            Destroy(gameObject);
+            
+
+        }
+    }
+}
---
+++ Teleport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider _col)
+    {
+        if (_col.gameObject.name == "Chicken")
+        {
+            _col.transform.position = Target.position;
+        }
+    }
+}
